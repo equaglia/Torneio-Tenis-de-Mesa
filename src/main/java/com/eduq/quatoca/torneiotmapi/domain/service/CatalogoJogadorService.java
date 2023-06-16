437d222e074bda167d56d8cc52c0477a015b77d8
equaglia <equaglia@gmail.com>
2023-06-16 17:56:34 -0300

    Eliminando interface CatalogoJogadorService

--- src/main/java/com/eduq/quatoca/torneiotmapi/domain/service/CatalogoJogadorService.java
+++ src/main/java/com/eduq/quatoca/torneiotmapi/domain/service/CatalogoJogadorService.java
@@ -1,19 +1,57 @@
 package com.eduq.quatoca.torneiotmapi.domain.service;
 
-import com.eduq.quatoca.torneiotmapi.domain.model.Jogador;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface CatalogoJogadorService {
-    Optional<Jogador> buscar(Long jogadorId);
+// import com.eduq.quatoca.torneiotmapi.domain.service.CatalogoJogadorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-    List<Jogador> listar();
+import com.eduq.quatoca.torneiotmapi.domain.exception.EntidadeNaoEncontradaException;
+import com.eduq.quatoca.torneiotmapi.domain.exception.NegocioException;
+import com.eduq.quatoca.torneiotmapi.domain.model.Jogador;
+import com.eduq.quatoca.torneiotmapi.domain.repository.JogadorRepository;
 
-    @Transactional
-    Jogador salvar(Jogador jogador);
+import lombok.AllArgsConstructor;
 
-    @Transactional
-    void excluir(Long jogadorId);
+@AllArgsConstructor
+@Service
+public class CatalogoJogadorService {
+
+	private JogadorRepository jogadorRepository;
+	
+	// @Override
+	public Optional<Jogador> buscar(Long jogadorId) {
+		return Optional.of(jogadorRepository.findById(jogadorId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Jogador não encontrado CatalogoJogadorService")));
+	}
+	
+	// @Override
+	public List<Jogador> listar() {
+		return jogadorRepository.findAll();
+	}
+
+	// @Override
+	@Transactional
+	public Jogador salvar(Jogador jogador) {
+		// TODO TBD controles, tipo: email, fone
+		return jogadorRepository.save(jogador);
+	}
+
+	// @Override
+	@Transactional
+	public void excluir(Long jogadorId) {
+		Jogador jogadorParaExcluir = this.buscar(jogadorId).orElse(null);
+
+		if (jogadorParaExcluir == null)
+			throw (new NegocioException("Jogador não existente"));
+		else if (jogadorParaExcluir.getPartidas().isEmpty()) {
+			jogadorRepository.deleteById(jogadorId);
+			new ResponseEntity<>("Jogador deletado do sistema", HttpStatus.NO_CONTENT);
+		} else {
+			throw(new NegocioException("Jogador com partida registrada não pode ser deletado."));
+		}
+	}
 }
